866a6bb2359070d2e3559f84ea8704312c2eb780
李有为 <525667108@qq.com>
2019-10-30 13:48:07 +0800

    随便试试

--- src/main/java/com/lyw/nall/controller/EsProductController.java
+++ src/main/java/com/lyw/nall/controller/EsProductController.java
@@ -59,6 +59,10 @@ public class EsProductController{
         EsProduct esProduct = esProductService.create(id);
         if (esProduct != null) {
             return CommonResult.success(esProduct);
+
+
+
+            
         } else {
             return CommonResult.failed();
         }
